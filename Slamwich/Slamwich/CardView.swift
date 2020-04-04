e0d62bdf931f54bffaed800d2491efa57081442e
Noah McLean <noahmclean@Noahs-MacBook-Pro.local>
2020-04-03 21:28:05 -0700

    fixed card nib, added pan gesture, need to iron out some bugs

--- Slamwich/Slamwich/CardView.swift
+++ Slamwich/Slamwich/CardView.swift
@@ -8,14 +8,60 @@
 
 import UIKit
 
+@IBDesignable
 class CardView: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    @IBOutlet var view: UIView!
+    var currentScale = 1.0
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupView()
     }
-    */
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setupView()
+    }
+    
+    // General setup
+    private func setupView() {
+        // Attach view to nib properly
+        let bundle = Bundle(for: CardView.self)
+        bundle.loadNibNamed("CardView", owner: self, options: nil)
+        
+        view.frame = bounds
+        view.autoresizingMask = [UIView.AutoresizingMask.flexibleWidth,
+                                 UIView.AutoresizingMask.flexibleHeight]
+        addSubview(view)
+        
+        // Set up minute card details
+        let layer = view.layer
+        layer.backgroundColor = CGColor(srgbRed: 1, green: 1, blue: 1, alpha: 0)
+        layer.borderColor = CGColor(srgbRed: 0, green: 0, blue: 0, alpha: 1)
+        layer.borderWidth = 1
+        layer.cornerRadius = 10
+        view.backgroundColor = .brown
+    }
+    
+    func scaleCard(scale: Double) {
+        
+        UIView.animate(withDuration: 0.2, delay: 0, options: .curveEaseInOut, animations: {
+
+            //print(self.frame)
+
+            //let offset: CGFloat = 25
+            /*
+            let offsetX = 2 * (offset / self.frame.width)
+            let offsetY = 2 * (offset * (self.frame.height / self.frame.width) / self.frame.height)
+ */
+            var transform = CGAffineTransform.identity
+            transform = transform.scaledBy(x: CGFloat(scale), y: CGFloat(scale))
+            self.transform = transform
+            self.currentScale = scale
+            //self.layer.position = CGPoint(x: offset, y: offset)
+
+        }, completion: nil)
+    }
 
 }
--- Slamwich/Slamwich/MainPlayerViewController.swift
+++ Slamwich/Slamwich/MainPlayerViewController.swift
@@ -10,17 +10,59 @@ import UIKit
 
 class MainPlayerViewController: UIViewController {
 
-    @IBOutlet weak var cardContainer: UIView!
+    @IBOutlet var cardSlots: [UIView]!
+    @IBOutlet weak var cardContainer: CardView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let layer = cardContainer.layer
-        layer.borderColor = CGColor(srgbRed: 0, green: 0, blue: 0, alpha: 1)
-        layer.borderWidth = 1
-        layer.cornerRadius = 10
+
+        let tap = UIPanGestureRecognizer(target: self, action: #selector(handlePan(_:)))
+        cardContainer.addGestureRecognizer(tap)
     }
 
+    @objc func handlePan(_ gesture: UIPanGestureRecognizer) {
+        let translation = gesture.translation(in: view)
+        
+        guard let gView = gesture.view else {
+            return
+        }
+        
+        guard let gestureView = gView as? CardView else {
+            return
+        }
 
+        if (gesture.state == .ended) {
+            let num = checkOverlap(cardView: gestureView)
+            if num == -1 {
+                gestureView.scaleCard(scale: 1.0)
+            }
+            else {
+                gestureView.scaleCard(scale: 0.75)
+            }
+        }
+        else if (gesture.state == .began) {
+            gestureView.scaleCard(scale: 1.1)
+        }
+                
+        gestureView.center = CGPoint(
+        x: gestureView.center.x + translation.x,
+        y: gestureView.center.y + translation.y
+        )
+
+        gesture.setTranslation(.zero, in: view)
+        
+        
+        }
+    
+    func checkOverlap(cardView: UIView) -> Int {
+        for slot in cardSlots {
+            if cardView.frame.intersects(slot.frame) {
+                return slot.tag
+            }
+        }
+        return -1
+    }
+
+        
 }
 
